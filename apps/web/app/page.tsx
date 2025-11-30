3b5de62c38f756d57f5d57878d6414f42af18725
leotrimhaliti <leotrimhalitti@gmail.com>
2025-11-30 16:58:30 +0100

    asdf

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex flex-1 flex-col items-center justify-center">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex lg:flex-col">
         <h1 className="text-4xl font-bold mb-8 tracking-tighter text-center">
-          commit.lol
+          Kommit.me
         </h1>
         <div className="mb-8 flex justify-center">
           <a href="/daily" className="text-sm text-muted-foreground hover:text-foreground underline underline-offset-4">View Daily Commit</a>
